acce342fbe213672f72f7960630ee3c39b0a0c51
Dima <akmeev2011@yandex.ru>
2022-12-03 20:40:12 +0300

    Initial commit

---
+++ src/main/java/com/example/algorithms_hw_2/AlgorithmsHw2Application.java
@@ -0,0 +1,13 @@
+package com.example.algorithms_hw_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsHw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsHw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/algorithms_hw_2/AlgorithmsHw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithms_hw_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsHw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
